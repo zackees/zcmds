ac732c263ee329b9c2b3a76507168c2edb0a0926
zackees <z@zackees.com>
2022-01-31 18:34:11 -0800

    nit

--- pysrc/cmds/common/shrink.py
+++ pysrc/cmds/common/shrink.py
@@ -1,17 +1,18 @@
 import os
-import argparse
 import sys
 from pathlib import Path
 
 def main():
+    if len(sys.argv != 2):
+        print(f'Expected two args, but got {len(sys.argv)} instead.')
+        sys.exit(1)
     filename = sys.argv[1:2][0]
-    #print(os.path.abspath(filename))
-    #return
     if not os.path.exists(filename):
         print(f'{filename} does not exist')
         sys.exit(1)
     out_path = Path('web_' + filename).with_suffix('.mp4')
-    os.system(f'ffmpeg -i "{filename}" -vf scale=640:-1 -c:v libx264 -crf 19 "{out_path}"')
+    cmd = f'ffmpeg -i "{filename}" -vf scale=640:-1 -c:v libx264 -crf 19 "{out_path}"'
+    os.system(cmd)
 
 if __name__ == "__main__":
     main()
