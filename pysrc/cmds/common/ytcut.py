6f064f3c1d880d25135f3c06b46e00d5044c4f24
zackees <z@zackees.com>
2022-02-17 20:47:08 -0800

    improves multithread pooling

--- pysrc/cmds/common/ytcut.py
+++ pysrc/cmds/common/ytcut.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-import threading
+import time
 import sys
 
 from concurrent.futures import ThreadPoolExecutor
@@ -8,7 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 def run_download_and_cut(url, outname, start_timestamp, length):
     stdout = subprocess.check_output(
-        f'yt-dlp --force-overwrites {url}', shell=True, universal_newlines=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+        f'yt-dlp --no-check-certificate --force-overwrites {url}', shell=True, universal_newlines=True, stderr=subprocess.PIPE)
     value = None
     for line in stdout.splitlines():
         needle = '[Merger] Merging formats into "'
@@ -19,7 +19,7 @@ def run_download_and_cut(url, outname, start_timestamp, length):
         raise OSError(f"Could not find a video in {stdout}")
     ffmpeg_cmd = f'ffmpeg -y -i "{value}" -ss {start_timestamp} -t {length} {outname}.mp4'
     stdout = subprocess.check_output(
-        ffmpeg_cmd, shell=True, universal_newlines=True, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+        ffmpeg_cmd, shell=True, universal_newlines=True, stderr=subprocess.PIPE)
     os.remove(value)
 
 
@@ -28,19 +28,18 @@ def main():
     executor = ThreadPoolExecutor(max_workers=8)
     try:
         while True:
-            print("Add new video (ctrl-c):")
+            print("Add new video:")
             url = input('  url: ')
-            output_name = input('  output_name: ')
             start_timestamp = input('  start_timestamp: ')
             length = input('  length: ')
-            t = threading.Thread(target=run_download_and_cut, args=(
-                url, output_name, start_timestamp, length))
+            output_name = input('  output_name: ')
+
             def task(): return run_download_and_cut(
                 url, output_name, start_timestamp, length)
             f = executor.submit(task)
             setattr(f, 'url', url)
             futures.append(f)
-            print(f"\nStarting {url} in background.")
+            print(f"\nStarting {url} in background.\n")
     except KeyboardInterrupt:
         pass
 
@@ -49,7 +48,8 @@ def main():
         print(f"\nWaiting for {len(unfinished_futures)} commands to finish")
         for i, f in enumerate(unfinished_futures):
             sys.stdout.write(f"  Waiting for {f.url} to finish...\n")
-            f.result()
+            while not f.done():
+                time.sleep(.1)
             sys.stdout.write(f"  ... done, {len(unfinished_futures)-i} left\n")
 
     print("All commands successfully completed.")
