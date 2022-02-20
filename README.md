65485a25125150b748317149fa6bc724c45b5fb7
zackees <z@zackees.com>
2022-02-19 22:42:37 -0800

    adds pdf2png and updates readme

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Cross platform(ish) productivity commands written in python
 
 Keep in mind that all files starting with "_" will be skipped.
+
+# Install:
+
+  * popplar (pdf2image)
+    * win32: `choco install poppler`
+    * ... ?
+  * `python -m pip install -r requirement.txt`
---
+++ pysrc/cmds/common/pdf2png.py
@@ -0,0 +1,30 @@
+from pdf2image import convert_from_path
+import os
+
+
+def main() -> None:
+    infile = input("input pdf: ")
+    page_start = int(input("first_page: "))
+    page_last = int(input("last page: "))
+    dpi = int(input("dpi: "))
+
+    save_dir = os.path.splitext(infile)[0]
+    os.makedirs(save_dir, exist_ok=True)
+    infile = save_dir + ".pdf"
+    images = convert_from_path(
+        infile,
+        first_page=page_start,
+        last_page=page_last,
+        dpi=dpi)
+    for i, image in enumerate(images):
+        output_png = os.path.join(save_dir, f'{i+page_start}.png')
+        if os.path.isfile(output_png):
+            os.remove(output_png)
+        print(f"Writing: {output_png}")
+        image.save(output_png)
+        if not os.path.isfile(output_png):
+            print(f"Error: could not save {output_png}")
+
+
+if __name__ == '__main__':
+    main()
