7cac7e9dcd3d797d4cb112aa6359ca8d5531febc
zackees <z@zackees.com>
2022-02-19 21:45:23 -0800

    adds crf quality control

--- pysrc/cmds/common/vidclip.py
+++ pysrc/cmds/common/vidclip.py
@@ -22,18 +22,20 @@ def main():
     parser.add_argument("--start_timestamp", help="start of the clip")
     parser.add_argument("--length", help="length of the clip")
     parser.add_argument("--outname", help="output name of the file")
+    parser.add_argument("--crf", default="18", type=int, help="crf quality of the file.")
     args = parser.parse_args()
 
     infile = args.input
     start_timestamp = args.start_timestamp or input("start_timestamp: ")
     length = args.length or input("length (seconds): ")
     output_path = args.outname or stripext(infile) + "_clip.mp4"
+    crf = args.crf  # The amount of quality, 0 is lossless and 50 is shit.
 
     print(infile)
     if not os.path.exists(infile):
         print(f"{infile} does not exist")
         sys.exit(1)
-    cmd = f'ffmpeg -i "{infile}" -c:v libx264 -crf 18 -ss {start_timestamp} -t {length} "{output_path}"'
+    cmd = f'ffmpeg -i "{infile}" -c:v libx264 -crf {crf} -ss {start_timestamp} -t {length} "{output_path}"'
     os.system(cmd)
     if not os.path.exists(output_path):
         print(f"Error, did not generate {output_path}")
