059d7a070e99cab0b3116d252632ff7a99d5e49b
zackees <z@zackees.com>
2022-02-20 18:49:17 -0800

    added zcmds update

--- productivity_cmds/cmds/common/zcmds.py
+++ productivity_cmds/cmds/common/zcmds.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+from productivity_cmds.cmds.common import update
 
 SELF_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.abspath(os.path.join(SELF_DIR, "..", ".."))
@@ -8,10 +11,15 @@ assert os.path.exists(BIN_DIR), f"could not find {BIN_DIR}"
 
 
 def main():
+    if "update" in sys.argv:
+        update.main()
+        return
     cmds = os.listdir(BIN_DIR)
     cmds = [os.path.splitext(c)[0] for c in cmds if c.endswith(".bat") or c.endswith(".exe")]
     cmds.append("ytclip")
     cmds = list(set(cmds))
+    cmds.remove('update')
+    cmds.append("zcmds update")
     cmds.sort()
     print("Commands:\n  " + "\n  ".join(cmds))
     return
