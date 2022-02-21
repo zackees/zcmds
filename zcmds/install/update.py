d9423c4e61675212cd1670c2c1943b57fe0ef136
zackees <z@zackees.com>
2022-02-21 01:56:19 -0800

    clears out bin directory everytime update is run

--- zcmds/install/update.py
+++ zcmds/install/update.py
@@ -6,11 +6,24 @@
 
 import sys
 import os
+import shutil
+
+from zcmds.paths import BIN_DIR
 
 SELF_DIR = os.path.dirname(__file__)
 
+def remove_everything_in_dir(dir_path: str) -> None:
+    if os.path.isdir(dir_path):
+        for file_name in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
 
 def main() -> None:
+    # delete everything in BIN_DIR, if it exists.
+    remove_everything_in_dir(BIN_DIR)
     if sys.platform == "darwin":
         from zcmds.install.darwin.update import main as darwin_update
 
