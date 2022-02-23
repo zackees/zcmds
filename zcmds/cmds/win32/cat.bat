92dc31b2992734101c24f79febe38f99a6c19517
zackees <z@zackees.com>
2022-02-23 02:52:43 -0800

    adds linux commands cat cp du grep mv sh

---
+++ zcmds/cmds/win32/cat.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Git\usr\bin\cat.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ zcmds/cmds/win32/git-bash.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.Popen(
+    r'"C:\Program Files\Git\git-bash.exe"',
+    shell=True,
+    start_new_session=True,
+    creationflags=subprocess.CREATE_NEW_CONSOLE,
+)
