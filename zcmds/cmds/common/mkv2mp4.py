0bf7496541675fbc10ec7ed0aa28b1c8c66f871a
zackees <z@zackees.com>
2022-02-23 02:48:37 -0800

    adds a bunch of unix commands from git-bash

--- zcmds/cmds/common/mkv2mp4.py
+++ zcmds/cmds/common/mkv2mp4.py
@@ -4,8 +4,6 @@ import os
 import sys
 from pathlib import Path
 
-from typing import List
-
 
 def main():
     if len(sys.argv) != 2:
--- zcmds/cmds/win32/ls.bat
+++ zcmds/cmds/win32/ls.bat
@@ -1,1 +1,2 @@
-dir %1
+@echo off
+"C:\Program Files\Git\usr\bin\ls.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
