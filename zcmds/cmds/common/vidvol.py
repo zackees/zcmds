367d57cda7a7d87fce80150eeedbfa4123e42592
zackees <z@zackees.com>
2022-02-24 00:22:05 -0800

    updates vid vol tool

--- zcmds/cmds/common/vidvol.py
+++ zcmds/cmds/common/vidvol.py
@@ -17,21 +17,35 @@ def ffmpeg_adjust_volume(filename: str, volume: float, out_file: str):
     """
     Adjusts the volume of a video file.
     """
-    cmd = f'ffmpeg -i "{filename}" -filter:a "volume={volume}" "{out_file}"'
+    cmd = f'static_ffmpeg  -i "{filename}" -filter:a "volume={volume}" "{out_file}"'
     print(f"Executing:\n  {cmd}")
+    os.system(cmd)
+
+
+def ffmpeg_volume_detect(filename: str) -> float:
+    """
+    Uses ffmpeg to get the volume of a video file.
+    """
+    cmd = (
+        f'static_ffmpeg -i "{filename}" -nostats -hide_banner -af "volumedetect" -f null /dev/null'
+    )
+    print("Executing:\n  " + cmd + "\n")
     os.system(cmd)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Print video durations\n",
+        description="Prints the video volume or sets it.\n",
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument("vidfile", help="Path to vid file", nargs="?")
-    parser.add_argument("out", help="Path to vid file", nargs="?")
-    parser.add_argument("--volume", help="Volume to adjust to", type=float)
+    parser.add_argument("--out", help="Path to vid file")
+    parser.add_argument("--volume", help="Volume to adjust to")
     args = parser.parse_args()
     vidfile = args.vidfile or input("in vid file: ")
+    if args.out is None and args.volume is None:
+        ffmpeg_volume_detect(vidfile)
+        return
     vol = args.volume or input("volume: ")
     out = args.out or input("out vid file: ")
     if len(os.path.dirname(vidfile)):
