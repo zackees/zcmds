74893fd3912ceb8557546391d88b175facb1edf2
zackees <z@zackees.com>
2022-02-24 01:10:58 -0800

    improves vidclip interactive and adds readme

---
+++ zcmds/cmds/common/README.md
@@ -0,0 +1,4 @@
+
+# Every python file here will become a command.
+
+Put shared code in zcmds/util/...
--- zcmds/cmds/common/vidclip.py
+++ zcmds/cmds/common/vidclip.py
@@ -1,8 +1,8 @@
 # pylint: skip-file
 
+import argparse
 import os
 import sys
-import argparse
 
 
 def sanitize(s: str) -> str:
@@ -19,29 +19,53 @@ def main():
         description="Cuts clips from local files.\n",
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument("input", help="input")
+    parser.add_argument("input", help="input", nargs="?")
     parser.add_argument("--start_timestamp", help="start of the clip")
     parser.add_argument("--length", help="length of the clip")
     parser.add_argument("--outname", help="output name of the file")
     parser.add_argument("--crf", default="18", type=int, help="crf quality of the file.")
     args = parser.parse_args()
 
-    infile = args.input
-    start_timestamp = args.start_timestamp or input("start_timestamp: ")
-    length = args.length or input("length (seconds): ")
-    output_path = args.outname or stripext(infile) + "_clip.mp4"
-    crf = args.crf  # The amount of quality, 0 is lossless and 50 is shit.
+    is_interactive = (
+        (args.start_timestamp is None)
+        and (args.length is None)
+        and (args.outname is None)
+        and (args.crf is None)
+    )
 
-    print(infile)
-    if not os.path.exists(infile):
-        print(f"{infile} does not exist")
-        sys.exit(1)
-    cmd = f'ffmpeg -i "{infile}" -c:v libx264 -crf {crf} -ss {start_timestamp} -t {length} "{output_path}"'
-    os.system(cmd)
-    if not os.path.exists(output_path):
-        print(f"Error, did not generate {output_path}")
-    else:
-        print(f"Generated: {output_path}")
+    infile = args.input or input("Input video: ")
+    ext = os.path.splitext(infile)[1]
+    count = 0
+
+    while True:
+        start_timestamp = args.start_timestamp or input("start_timestamp: ")
+        length = args.length or input("length (seconds): ")
+        start_timestamp_str = sanitize(start_timestamp)
+        length_str = sanitize(length)
+        output_path = (
+            args.outname or stripext(infile) + f"_clip_{start_timestamp_str}_{length_str}{ext}"
+        )
+        count += 1
+        crf = args.crf  # The amount of quality, 0 is lossless and 50 is shit.
+
+        args.start_timestamp = None
+        args.length = None
+        args.outname = None
+
+        print(infile)
+        if not os.path.exists(infile):
+            print(f"{infile} does not exist")
+            sys.exit(1)
+        cmd = f'ffmpeg -i "{infile}" -c:v libx264 -crf {crf} -ss {start_timestamp} -t {length} "{output_path}"'
+        os.system(cmd)
+        if not os.path.exists(output_path):
+            print(f"Error, did not generate {output_path}")
+        else:
+            print(f"Generated: {output_path}")
+        if not is_interactive:
+            break
+        if "y" in input("Continue? (y/n): ").lower():
+            break
 
 
 if __name__ == "__main__":
