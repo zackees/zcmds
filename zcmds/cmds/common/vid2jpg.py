72d0ad2cc6d64ecc868118ee5889813b078f3b03
zackees <z@zackees.com>
2022-03-09 18:30:15 -0800

    improves vd2jpg and silences warnings

--- zcmds/cmds/common/vid2jpg.py
+++ zcmds/cmds/common/vid2jpg.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import subprocess
 import sys
 
 
@@ -16,12 +17,12 @@ def main():
     )
     parser.add_argument("input", help="input")
     parser.add_argument("--timestamp", help="start of the clip")
-    parser.add_argument("--length", help="length of the clip", default="1")
+    parser.add_argument("--length", help="length of the clip")
     parser.add_argument("--outname", help="output name of the file")
     args = parser.parse_args()
     infile = args.input
     timestamp = args.timestamp or input("timestamp: ")
-    length = args.length
+    length = args.length or input("length (secs): ")
     if args.outname:
         output_path = os.path.splitext(args.outname)[0]
     else:
@@ -38,7 +39,7 @@ def main():
 
     cmd = f'static_ffmpeg -hide_banner -i "{infile}" -ss {timestamp} -t {length} -f image2 {file_output_fmt}'
     print(f"Executing:\n  {cmd}\n")
-    os.system(cmd)
+    subprocess.call(cmd, shell=True, universal_newlines=True, stderr=subprocess.DEVNULL)
     if not os.path.exists(output_path):
         print(f"Error, did not generate {output_path}")
         print(f"Error from cmd:\n  {cmd}\n")
