470931b1fe49bfc8b177f2c1fe03ddb55d745494
zackees <z@zackees.com>
2022-04-10 13:42:52 -0700

    fixes unicode decode error

--- zcmds/util/fileutils.py
+++ zcmds/util/fileutils.py
@@ -5,6 +5,7 @@
 import argparse
 import fnmatch
 import os
+import sys
 
 
 def get_search_args(require_replace_args=False):
@@ -34,7 +35,11 @@ def iter_matching_files(cur_dir, file_pattern, text_search_string=None):
                     yield full_path
                 else:
                     with open(full_path, encoding="utf-8") as fd:  # pylint: disable=invalid-name
-                        file_data = fd.read()
+                        try:
+                            file_data = fd.read()
+                        except UnicodeDecodeError:
+                            sys.stderr.write(f"  {__file__}: Could not read file: {full_path}\n")
+                            continue
                     if text_search_string in file_data:
                         yield full_path
 
