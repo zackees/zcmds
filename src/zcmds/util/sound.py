d317acc98ad7f27511d1a913c61c3c98a9b6553c
zackees <zachvorhies@protonmail.com>
2023-01-23 22:36:36 -0500

    try and fix win32

--- src/zcmds/util/sound.py
+++ src/zcmds/util/sound.py
@@ -12,7 +12,14 @@ def beep() -> None:
     """Play a beep sound."""
 
     assert os.path.exists(SOUND_FILE), f"Sound file {SOUND_FILE} does not exist."
-    playsound(SOUND_FILE, block=True)
+    prev_cwd = os.getcwd()
+    basedir = os.path.dirname(SOUND_FILE)
+    os.chdir(basedir)
+    file_name = os.path.basename(SOUND_FILE)
+    try:
+        playsound(file_name, block=True)
+    finally:
+        os.chdir(prev_cwd)
 
 
 if __name__ == "__main__":
