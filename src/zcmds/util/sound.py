273f0b2eb461370b9efedeb01a878277b691c932
zackees <zachvorhies@protonmail.com>
2023-01-23 23:54:58 -0500

    remove extranous dependencies

--- src/zcmds/util/sound.py
+++ src/zcmds/util/sound.py
@@ -1,16 +1,7 @@
 """Sound utilities."""
 
-# pylint: disable=all
-# mypy: ignore-errors
-# flake8: noqa
-
 import os
 import sys
-import tempfile
-
-from playsound import playsound  # type: ignore
-from pydub import AudioSegment  # type: ignore
-from static_ffmpeg import add_paths  # type: ignore
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 SOUND_FILE = os.path.abspath(os.path.join(HERE, "..", "assets", "bell.mp3"))
@@ -25,9 +16,11 @@ def do_playsound(file: str) -> None:
     assert os.path.exists(SOUND_FILE), f"Sound file {SOUND_FILE} does not exist."
     ext = os.path.splitext(file)[1]
     if sys.platform != "win32" or ext == ".wav":
+        from playsound import playsound  # type: ignore  # pylint: disable=all
+
         playsound(file, block=True)
         return
-    import winsound  # type: ignore
+    import winsound  # type: ignore  # pylint: disable=all
 
     winsound.PlaySound(file, winsound.SND_FILENAME)
 
