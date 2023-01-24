a727f24bb6915435f0427739a04bcad6a397b8fd
zackees <zachvorhies@protonmail.com>
2023-01-23 22:56:58 -0500

    try and fix win32

--- src/zcmds/util/sound.py
+++ src/zcmds/util/sound.py
@@ -1,8 +1,10 @@
 """Sound utilities."""
 
 import os
+import tempfile
 
 from playsound import playsound  # type: ignore
+from pydub import AudioSegment  # type: ignore
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 SOUND_FILE = os.path.abspath(os.path.join(HERE, "..", "assets", "bell.mp3"))
@@ -12,14 +14,23 @@ def beep() -> None:
     """Play a beep sound."""
 
     assert os.path.exists(SOUND_FILE), f"Sound file {SOUND_FILE} does not exist."
+    do_playsound(SOUND_FILE)
+
+
+def do_playsound(file: str) -> None:
     prev_cwd = os.getcwd()
-    basedir = os.path.dirname(SOUND_FILE)
+    basedir = os.path.dirname(file)
     os.chdir(basedir)
-    file_name = os.path.basename(SOUND_FILE)
-    try:
-        playsound(file_name, block=True)
-    finally:
-        os.chdir(prev_cwd)
+    file_name = os.path.basename(file)
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dst = os.path.join(tmpdir, "tmp.wav")
+        sound = AudioSegment.from_mp3(file_name)
+        sound.export(dst, format="wav")
+        try:
+            playsound(dst, block=True)
+        finally:
+            os.chdir(prev_cwd)
 
 
 if __name__ == "__main__":
