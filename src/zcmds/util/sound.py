e2b6a0fd542122330631f67c9ee8452e303b6c8b
zackees <zachvorhies@protonmail.com>
2023-01-24 13:22:58 -0500

    update playaudio lib

--- src/zcmds/util/sound.py
+++ src/zcmds/util/sound.py
@@ -1,12 +1,12 @@
 """Sound utilities."""
 
 from playaudio import playaudio  # type: ignore
-from playaudio.assets.builtin import BELL_FILE  # type: ignore
+from playaudio.assets.builtin import BELL_MP3  # type: ignore
 
 
 def beep() -> None:
     """Play a beep sound."""
-    playaudio(BELL_FILE)
+    playaudio(BELL_MP3)
 
 
 if __name__ == "__main__":
