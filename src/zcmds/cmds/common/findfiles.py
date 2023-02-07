2680571561f991ae7b96f704d4623522a136b5a5
zackees <z@zackees.com>
2023-02-07 12:55:28 -0800

    nit

--- src/zcmds/cmds/common/findfiles.py
+++ src/zcmds/cmds/common/findfiles.py
@@ -8,29 +8,29 @@ import os
 
 
 def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="glob file name", nargs="?")
-    parser.add_argument("--cwd", help="current working directory", default=os.getcwd())
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("file", help="glob file name", nargs="?")
+        parser.add_argument("--cwd", help="current working directory", default=os.getcwd())
+        args = parser.parse_args()
 
-    file: str = os.path.expanduser(args.file) if args.file else input("File: ")
-    # trim file
-    args.file = file.strip()
-    found = False
-    for root, _, files in os.walk(os.getcwd()):
-        for name in files:
-            if glob.fnmatch.fnmatch(name, file):  # type: ignore
-                print(os.path.join(root, name))
-                found = True
-    if not found:
-        print("File not found")
+        file: str = os.path.expanduser(args.file) if args.file else input("File: ")
+        # trim file
+        args.file = file.strip()
+        found = False
+        for root, _, files in os.walk(os.getcwd()):
+            for name in files:
+                if glob.fnmatch.fnmatch(name, file):  # type: ignore
+                    print(os.path.join(root, name))
+                    found = True
+        if not found:
+            print("File not found")
+            return 1
+        return 0
+    except KeyboardInterrupt:
+        print("Aborted")
         return 1
-    return 0
 
 
 if __name__ == "__main__":
-    try:
-        exit(main())
-    except KeyboardInterrupt:
-        print("Aborted")
-        exit(1)
+    exit(main())
