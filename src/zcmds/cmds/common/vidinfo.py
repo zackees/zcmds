2b52d510139ace0576ec62eacb0409fa96ddb169
zackees <z@zackees.com>
2023-03-12 19:17:01 -0700

    improve vidinfo and execution time

--- src/zcmds/cmds/common/vidinfo.py
+++ src/zcmds/cmds/common/vidinfo.py
@@ -1,6 +1,7 @@
 # pylint: skip-file
 
 import argparse
+import json
 import os
 import subprocess
 import sys
@@ -19,88 +20,6 @@ def exec(cmd: str) -> Tuple[int, str, str]:
     return proc.returncode, stdout, stderr
 
 
-def get_video_encoder(vidfile: str) -> str:
-    """Returns the encoder used for the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams v:0 -show_entries stream=codec_name"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-
-
-def get_audio_encoder(vidfile: str) -> str:
-    """Returns the encoder used for the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams a:0 -show_entries stream=codec_name"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-
-
-def get_video_height(vidfile: str) -> int:
-    """Returns the height of the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams v:0 -show_entries stream=height"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return int(
-        subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-    )
-
-
-def get_video_width(vidfile: str) -> int:
-    """Returns the width of the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams v:0 -show_entries stream=width"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return int(
-        subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-    )
-
-
-def get_video_duration(vidfile: str) -> float:
-    """Returns the duration of the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams v:0 -show_entries stream=duration"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return float(
-        subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-    )
-
-
-def get_audio_bitrate(vidfile: str) -> int:
-    """Returns the bitrate of the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return int(
-        subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-    )
-
-
-def get_audio_channels(vidfile: str) -> int:
-    """Returns the number of channels of the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams a:0 -show_entries stream=channels"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return int(
-        subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-    )
-
-
-def get_video_bitrate(vidfile: str) -> str:
-    """Returns the bitrate of the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-
-
 def format_duration(duration: float) -> str:
     """Returns a string representation of the given duration."""
     hours = int(duration / 3600)
@@ -116,19 +35,84 @@ def format_duration(duration: float) -> str:
     return out
 
 
-def get_frame_count(vidfile: str) -> str:
-    """Returns the number of frames of the given video file."""
-    cmd = (
-        "static_ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames"
-        f' -of default=nokey=1:noprint_wrappers=1 "{vidfile}"'
-    )
-    return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
-
-
 def get_format_json(vidfile: str) -> str:
     """Returns the format json of the given video file."""
-    cmd = f'static_ffprobe -v error -print_format json -show_format -show_streams "{vidfile}"'
-    return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
+    cmd = f'static_ffprobe -v quiet -print_format json -show_format -show_streams "{vidfile}"'
+    json_str = subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
+    json_data = json.loads(json_str)
+    json_str = json.dumps(json_data, indent=4)
+    return json_str
+
+
+def get_videostream_info(videstream: dict) -> str:
+    """Returns a string representation of the given video stream."""
+    lines = []
+    lines.append(f"  Video:")
+    lines.append(f"    Encoder: {videstream['codec_long_name']}, {videstream['codec_tag_string']}")
+    # pix format
+    pix_fmt = videstream["pix_fmt"]
+    lines.append(f"    Pixel format: {pix_fmt}")
+    has_bframes = int(videstream.get("has_b_frames", "0"))
+    if has_bframes:
+        lines.append(f"    B-Frames: {has_bframes}")
+    lines.append(f"    Height: {videstream['height']}")
+    lines.append(f"    Width: {videstream['width']}")
+    lines.append(f"    Duration: {format_duration(float(videstream['duration']))}")
+    video_bitrate = videstream["bit_rate"]
+    if video_bitrate.isdigit():
+        video_bitrate = int(video_bitrate)
+        video_bitrate_str = f"{video_bitrate / 1000000:.2f} Mbps"
+    lines.append(f"    Bitrate: {video_bitrate_str}")
+    lines.append(f"    Frame count: {videstream['nb_frames']}")
+    fr_num = int(videstream["r_frame_rate"].split("/")[0])
+    fr_den = int(videstream["r_frame_rate"].split("/")[1])
+    framerate = float(fr_num) / float(fr_den)
+    lines.append(f"    Framerate: {framerate:.2f} fps")
+    return "\n".join(lines)
+
+
+def get_audiostream_info(audiostream: dict) -> str:
+    """Returns a string representation of the given audio stream."""
+    lines = []
+    lines.append(f"  Audio:")
+    lines.append(
+        f"    Encoder: {audiostream['codec_long_name']}, {audiostream['codec_tag_string']}"
+    )
+    lines.append(f"    Channels: {audiostream['channels']}")
+    lines.append(f"    Duration: {format_duration(float(audiostream['duration']))}")
+    lines.append(f"    Sample rate: {audiostream['sample_rate']}")
+    audio_bitrate = audiostream["bit_rate"]
+    if audio_bitrate.isdigit():
+        audio_bitrate = int(audio_bitrate)
+        audio_bitrate_str = f"{audio_bitrate / 1000:.2f} kbps"
+    lines.append(f"    Bitrate: {audio_bitrate_str}")
+    return "\n".join(lines)
+
+
+def print_short_info(vidfile: str) -> None:
+    """Prints a short info about the given video file."""
+    filesize = os.path.getsize(vidfile)
+    filesize_str = f"{filesize / 1000000:.2f} MB"
+    print(f"File: {vidfile}")
+    print(f"  Size: {filesize_str}")
+    vidinfo_data = json.loads(get_format_json(vidfile))
+    streams = vidinfo_data.get("streams", [])
+    has_video_stream = False
+    has_audio_stream = False
+    for i, stream in enumerate(streams):
+        print(f"Track {i}:")
+        if stream["codec_type"] == "video":
+            has_video_stream = True
+            print(get_videostream_info(stream))
+        elif stream["codec_type"] == "audio":
+            has_audio_stream = True
+            print(get_audiostream_info(stream))
+    if not has_video_stream or not has_audio_stream:
+        print("")
+    if not has_video_stream:
+        print("(No video stream found)")
+    if not has_audio_stream:
+        print("(No audio stream found)")
 
 
 def main():
@@ -148,43 +132,10 @@ def main():
         sys.exit(1)
     if not args.full:
         try:
-            video_encoder = get_video_encoder(infile)
-            video_height = get_video_height(infile)
-            video_width = get_video_width(infile)
-            video_duration = get_video_duration(infile)
-            video_duration_str = format_duration(video_duration)
-            video_bitrate = get_video_bitrate(infile)
-            # if is integer
-            if video_bitrate.isdigit():
-                video_bitrate = int(video_bitrate)
-                video_bitrate_str = f"{video_bitrate / 1000000:.2f} Mbps"
-            else:
-                video_bitrate_str = f"{video_bitrate}"
-            video_frame_count = get_frame_count(infile)
-            print("Video:")
-            print(f"  Encoder: {video_encoder}")
-            print(f"  Height: {video_height}")
-            print(f"  Width: {video_width}")
-            print(f"  Duration: {video_duration_str}")
-            print(f"  Bitrate: {video_bitrate_str}")
-            print(f"  Frame count: {video_frame_count}")
+            print_short_info(infile)
         except subprocess.CalledProcessError:
             print("No video stream found")
 
-        try:
-            audio_encoder = get_audio_encoder(infile)
-            if not audio_encoder:
-                print("No audio stream found")
-            else:
-                audio_bitrate = get_audio_bitrate(infile)
-                audio_bitrate_str = f"{audio_bitrate / 1000:.0f} kbps"
-                audio_channels = get_audio_channels(infile)
-                print("Audio:")
-                print(f"  Encoder: {audio_encoder}")
-                print(f"  Bitrate: {audio_bitrate_str}")
-                print(f"  Channels: {audio_channels}")
-        except subprocess.CalledProcessError:
-            print("No audio stream found")
     else:
         json_str = get_format_json(infile)
         print(json_str)
