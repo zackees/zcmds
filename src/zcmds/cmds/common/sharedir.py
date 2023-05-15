5164bbd9b2f21c7924ef7e1c3c18a6447a44ab87
zackees <z@zackees.com>
2023-05-14 22:31:59 -0700

    fix sharedirs

--- src/zcmds/cmds/common/sharedir.py
+++ src/zcmds/cmds/common/sharedir.py
@@ -1,3 +1,13 @@
 import os
+import sys
 
-os.system("ngrok http file:///")
+
+def main() -> int:
+    try:
+        return os.system("ngrok http file:///")
+    except KeyboardInterrupt:
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
