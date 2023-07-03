a93c62caa5a6f0cc0c94d0551e697c6e921b9ccd
zackees <z@zackees.com>
2023-07-02 20:55:07 -0700

    Adds "myip"

---
+++ src/zcmds/cmds/common/myip.py
@@ -0,0 +1,23 @@
+from typing import Optional
+import requests
+import sys
+
+
+def get_public_ip4() -> Optional[str]:
+    urls = ["https://api.ipify.org", "https://ipv4.icanhazip.com/"]
+    for i, url in enumerate(urls):
+        try:
+            return requests.get(url, timeout=5).text.strip()
+        except requests.exceptions.RequestException:
+            pass
+    print("Unable to get public ip4")
+    return None
+
+
+def main() -> int:
+    print(get_public_ip4())
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
