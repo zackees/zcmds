2421f3567d6ac8b6a0e1ab88742e8faf77dce85f
zackees <z@zackees.com>
2023-07-09 20:18:52 -0700

    askai

--- src/zcmds/util/prompt_input.py
+++ src/zcmds/util/prompt_input.py
@@ -1,15 +1,26 @@
 """Prompts for input until two empty lines are entered."""
 
+import time
+
 
 def prompt_input() -> str:
     lines: list[str] = []
     empty_count = 0
+    response_times: list[float] = []
     while True:
+        start_time = time.time()
         line = input(">>> ")
+        diff_time = time.time() - start_time
+        response_times.append(diff_time)
         if not line:
             empty_count += 1
             if empty_count == 2:
-                break
+                last_two = response_times[-2:]
+                avg = sum(last_two) / len(last_two)
+                if avg < 0.1:
+                    empty_count = 0
+                else:
+                    break
         else:
             empty_count = 0
         lines.append(line)
