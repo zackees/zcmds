2c9b19561b5ddf93cd302571e010cf22cabac478
zackees <z@zackees.com>
2023-12-09 17:42:47 -0800

    fix missing executables

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import re
 import setuptools
 
 URL = "https://github.com/zackees/zcmds"
-
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -23,6 +22,7 @@ def get_readme() -> str:
 
 
 def get_console_scripts() -> list[str]:
+    """Get console scripts from cmds.txt"""
     cmds_txt = os.path.join(HERE, "src", "zcmds", "cmds.txt")
     with open(cmds_txt, encoding="utf-8", mode="r") as cmds_file:
         cmds = cmds_file.readlines()
@@ -32,13 +32,22 @@ def get_console_scripts() -> list[str]:
 
 if __name__ == "__main__":
     setuptools.setup(
-        maintainer="Zachary Vorhies",
+        name="zcmds",
+        version="1.4.30",
+        description="Cross platform(ish) productivity commands written in python.",
         long_description=get_readme(),
         long_description_content_type="text/markdown",
+        maintainer="Zachary Vorhies",
         url=URL,
+        license="BSD 3-Clause License",
+        classifiers=["Programming Language :: Python :: 3"],
+        python_requires=">=3.7",
+        install_requires=[  # Assuming you have dependencies listed in requirements.txt
+            requirement.strip() for requirement in open("requirements.txt").readlines()
+        ],
         package_data={"": ["assets/bell.mp3"]},
         include_package_data=True,
         entry_points={
-            "console_scripts": [get_console_scripts()],
+            "console_scripts": get_console_scripts(),
         },
     )
