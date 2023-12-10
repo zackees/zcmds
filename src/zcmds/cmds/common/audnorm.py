04e85af5504a7cbc829dd727d7c2a468f75a07e9
zackees <z@zackees.com>
2023-12-09 17:28:26 -0800

    improve aud norm with static sox

--- src/zcmds/cmds/common/audnorm.py
+++ src/zcmds/cmds/common/audnorm.py
@@ -7,6 +7,7 @@ import os
 import subprocess
 
 from static_ffmpeg import add_paths  # type: ignore
+from static_sox import add_paths as add_paths_sox  # type: ignore
 
 
 def ffprobe_duration(filename: str) -> float:
@@ -27,6 +28,47 @@ def _is_media_file(filename: str) -> bool:
     return ext in [".mp4", ".mkv", ".avi", ".mov", ".mp3", ".wav"]
 
 
+def _convert_to_wav(path: str, out: str) -> None:
+    """
+    Converts a media file to a wav file.
+    """
+    assert _is_media_file(path), f"{path} is not a media file"
+    if len(os.path.dirname(out)):
+        os.makedirs(os.path.dirname(out), exist_ok=True)
+    add_paths(weak=True)
+    cmd = f'ffmpeg -i "{path}" -vn -acodec pcm_s16le -ar 44100 -ac 2 "{out}"'
+    print(f"Executing:\n  {cmd}")
+    os.system(cmd)
+
+
+def _convert_to_mp3(path: str, out: str) -> None:
+    """
+    Converts a media file to an AAC file with 192k bitrate.
+    """
+    assert _is_media_file(path), f"{path} is not a media file"
+    if len(os.path.dirname(out)):
+        os.makedirs(os.path.dirname(out), exist_ok=True)
+    add_paths(weak=True)
+    # Updated FFmpeg command for AAC conversion
+    cmd = f'ffmpeg -i "{path}" -vn -acodec aac -b:a 192k "{out}"'
+    print(f"Executing:\n  {cmd}")
+    os.system(cmd)
+
+
+def _replace_audio(in_vid_mp4: str, in_mp3: str, out_mp3) -> None:
+    """
+    Replaces the audio of a video file with an mp3 file.
+    """
+    assert _is_media_file(in_vid_mp4), f"{in_vid_mp4} is not a media file"
+    assert _is_media_file(in_mp3), f"{in_mp3} is not a media file"
+    if len(os.path.dirname(out_mp3)):
+        os.makedirs(os.path.dirname(out_mp3), exist_ok=True)
+    add_paths(weak=True)
+    cmd = f'ffmpeg -i "{in_vid_mp4}" -i "{in_mp3}" -map 0:v -map 1:a -c copy -shortest "{out_mp3}"'
+    print(f"Executing:\n  {cmd}")
+    os.system(cmd)
+
+
 def audnorm(path: str, out: str) -> None:
     """
     Normalizes the audio of a video file.
@@ -35,10 +77,22 @@ def audnorm(path: str, out: str) -> None:
     if len(os.path.dirname(out)):
         os.makedirs(os.path.dirname(out), exist_ok=True)
     add_paths(weak=True)
+    add_paths_sox(weak=True)
+    out_wav = f"{out}.wav"
+    out_wav_norm = f"{out}.wav.norm"
+    out_mp3 = f"{out}.mp3"
+    _convert_to_wav(path, out_wav)
+
+    sox_cmd = f'sox "{out_wav}" "{out_wav_norm}" norm'
+    print(f"Executing:\n  {sox_cmd}")
+    os.system(sox_cmd)
     # Note that aac is supported by twitter, lame is not.
-    cmd = f'ffmpeg-normalize -f "{path}" -o "{out}" -c:a aac -b:a 192k'
-    print(f"Executing:\n  {cmd}")
-    os.system(cmd)
+    # cmd = f'ffmpeg-normalize -f "{path}" -o "{out}" -c:a aac -b:a 192k'
+    # print(f"Executing:\n  {cmd}")
+    _convert_to_mp3(out_wav_norm, out_mp3)
+
+    # now mix in new audio with old video
+    _replace_audio(path, out_mp3, out)
 
 
 def main():
