582b88389041a17fe3d1308bdcf3e1e4b70b8c65
zackees <z@zackees.com>
2023-12-17 19:06:05 -0800

    update wheel for upload package

--- upload_package.sh
+++ upload_package.sh
@@ -2,6 +2,7 @@
 set -e
 rm -rf build dist
 echo "Building Source and Wheel (universal) distribution…"
+pip install twine wheel
 python setup.py sdist bdist_wheel --universal
 echo "Uploading the package to PyPI via Twine…"
 twine upload dist/* --verbose
