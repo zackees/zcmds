6a42ed539e92776559767ef0134f3a4b564bd6c9
zackees <z@zackees.com>
2024-01-23 18:18:08 -0800

    bump to 1.4.58

--- src/zcmds/version.py
+++ src/zcmds/version.py
@@ -1,5 +1,5 @@
 """Just holds the version for the app"""
 
 # Also change version in pyproject.toml
-VERSION = "1.4.57"  # pylint: disable=R0801
+VERSION = "1.4.58"  # pylint: disable=R0801
 __version__ = VERSION
