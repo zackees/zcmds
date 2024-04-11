eee44a6809bf25ae58ec54e418839ec99cdf924d
zackees <z@zackees.com>
2024-04-11 12:54:39 -0700

    Add dataclass for AiderUpdateResult

--- src/zcmds/cmds/common/aicode.py
+++ src/zcmds/cmds/common/aicode.py
@@ -7,8 +7,11 @@ import re
 import shutil
 import subprocess
 import sys
+import time
 import warnings
-from typing import Tuple
+from dataclasses import dataclass
+from threading import Thread
+from typing import Optional, Tuple, Union
 
 from zcmds.cmds.common.openaicfg import create_or_load_config, save_config
 
@@ -24,7 +27,9 @@ def install_aider_if_missing() -> None:
     if shutil.which("aider") is not None:
         return
     print("Installing aider...")
-    os.system("pipx install aider-chat")
+    rtn = os.system("pipx install aider-chat")
+    if rtn != 0:
+        assert False, "Failed to install aider"
     assert shutil.which("aider") is not None, "aider not found after install"
 
 
@@ -126,7 +131,39 @@ def extract_version_string(version_string: str) -> str:
     raise ValueError(f"Failed to extract version string from {version_string}")
 
 
-def aider_check_update() -> None:
+@dataclass
+class AiderUpdateResult:
+    has_update: bool
+    latest_version: str
+    current_version: str
+    error: Optional[str] = None
+
+    def get_update_msg(self) -> str:
+        msg = "\n#######################################\n"
+        msg += f"# UPDATE AVAILABLE: {self.current_version} -> {self.latest_version}.\n"
+        msg += "# run `aicode --upgrade` to upgrade\n"
+        msg += "#######################################\n"
+        return msg
+
+    def to_json_data(self) -> dict[str, Union[str, bool, None]]:
+        return {
+            "has_update": self.has_update,
+            "latest_version": self.latest_version,
+            "current_version": self.current_version,
+            "error": str(self.error) if self.error is not None else None,
+        }
+
+    @classmethod
+    def from_json(cls, json_data: dict[str, Union[str, bool]]) -> "AiderUpdateResult":
+        return AiderUpdateResult(
+            has_update=bool(json_data["has_update"]),
+            latest_version=str(json_data["latest_version"]),
+            current_version=str(json_data["current_version"]),
+            error=str(json_data["error"]) if json_data["error"] is not None else None,
+        )
+
+
+def aider_check_update() -> AiderUpdateResult:
     # rtn = os.system("aider --check-update")
     try:
         cp = subprocess.run(
@@ -136,27 +173,21 @@ def aider_check_update() -> None:
             universal_newlines=True,
         )
         if cp.returncode == 0:
-            return
+            return AiderUpdateResult(False, "", "")
     except Exception:  # pylint: disable=broad-except
-        return
+        return AiderUpdateResult(False, "", "")
     stdout = cp.stdout.strip()
     lines = stdout.split("\n")
     try:
-        current_version = extract_version_string(lines[0])
-        latest_version = extract_version_string(lines[1])
-        print(
-            "\n#######################################\n"
-            f"# UPDATE AVAILABLE: {current_version} -> {latest_version}.\n"
-            "# run `aicode --upgrade` to upgrade\n"
-            "#######################################\n"
-        )
-        # if input("Upgrade now? [y/N] ").lower() == "y":
-        #    upgrade_aider()
-        return
+        current_version: str = extract_version_string(lines[0])
+        latest_version: str = extract_version_string(lines[1])
+        out = AiderUpdateResult(True, latest_version, current_version)
+        # print(out.get_update_msg())
+        return out
     except Exception as err:  # pylint: disable=broad-except
         warnings.warn(f"Failed to parse update message: {err}")
         pass
-    print(f"\nUPDATE AVAILABLE: {stdout}, run `aicode --upgrade` to upgrade\n\n")
+    return AiderUpdateResult(True, "Unknown", "Unknown")
 
 
 def check_gitignore() -> None:
@@ -187,14 +218,34 @@ def check_gitignore() -> None:
         print(".gitignore file does not exist.")
 
 
+def background_update_task(config: dict) -> None:
+    try:
+        # Wait for aider to start so that we don't impact startup time.
+        # This is really needed for windows because startup is so slow.
+        time.sleep(5)
+        update_info = aider_check_update()
+        if update_info.has_update:
+            config["aider_update_info"] = update_info.to_json_data()
+            save_config(config)
+        else:
+            config["aider_update_info"] = {}
+            save_config(config)
+    except KeyboardInterrupt:
+        pass
+    except SystemExit:
+        pass
+
+
 def cli() -> int:
     check_gitignore()
     args, unknown_args = parse_args()
+    config = create_or_load_config()
     if args.upgrade:
         upgrade_aider()
+        config["aider_update_info"] = {}  # Purge stale update info
+        save_config(config)
         return 0
-    aider_check_update()
-    config = create_or_load_config()
+
     if args.set_key:
         config["openai_key"] = args.set_key
         save_config(config)
@@ -205,6 +256,27 @@ def cli() -> int:
     if openai_key is None:
         print("OpenAI key not found, please set one with --set-key")
         return 1
+
+    last_aider_update_info: dict[str, Union[str, bool]] = config.get(
+        "aider_update_info", {}
+    )
+    update_info: Optional[AiderUpdateResult] = None
+    if last_aider_update_info:
+        try:
+            update_info = AiderUpdateResult.from_json(last_aider_update_info)
+            if update_info.error:
+                warnings.warn(f"Failed to parse update info: {update_info.error}")
+                update_info = None
+        except Exception as err:  # pylint: disable=broad-except
+            warnings.warn(f"Failed to parse update info: {err}")
+            update_info = None
+
+    if update_info is not None and update_info.has_update:
+        print(update_info.get_update_msg())
+
+    # TODO: Remove AIDER_MODEL for default value. This used to be needed
+    # for the old version of aider which used Chat GPT 3.5 as the default. Now
+    # Aider uses one of the ChatGPT 4 model variants as the default.
     os.environ["AIDER_MODEL"] = model
     print(f"Starting aider with model {os.environ['AIDER_MODEL']}")
     os.environ["OPENAI_API_KEY"] = openai_key
@@ -215,6 +287,11 @@ def cli() -> int:
         cmd_list.append("--no-auto-commit")
     cmd_list += args.prompt + unknown_args
     print("\nLoading aider:\n  remember to use /help for a list of commands\n")
+    # Perform update in the background.
+    update_thread = Thread(target=background_update_task, args=(config,))
+    update_thread.daemon = True
+    update_thread.start()
+
     rtn = subprocess.call(cmd_list)
     if args.keep:
         return rtn
