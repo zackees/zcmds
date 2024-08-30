e12e1be4699f829313de18201bcd217ee6450d4a
zackees <z@zackees.com>
2024-08-30 15:06:49 -0700

    add -y to vidclip static_ffmpeg

--- src/zcmds/cmds/common/vidclip.py
+++ src/zcmds/cmds/common/vidclip.py
@@ -52,7 +52,7 @@ def encode(
         from_stmt = f"-ss {start_timestamp}"
     if end_timestamp:
         to_stmt = f"-to {end_timestamp}"
-    cmd = f'static_ffmpeg -hide_banner -i "{infile}" -c:v libx264 {vf_scale_part} -preset veryslow -crf {crf} {from_stmt} {to_stmt} "{output_path}"'
+    cmd = f'static_ffmpeg -hide_banner -i -y "{infile}" -c:v libx264 {vf_scale_part} -preset veryslow -crf {crf} {from_stmt} {to_stmt} "{output_path}"'
     print_fcn(f"Executing:\n  {cmd}\n")
     rtn, _, _ = exec(cmd)
     if rtn != 0:
